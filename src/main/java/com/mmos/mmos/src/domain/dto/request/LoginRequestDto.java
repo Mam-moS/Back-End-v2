5bcf7952fab2d323d59cd7fc966f242eec3c94db
KoungQ <guu119@naver.com>
2024-02-25 17:29:54 +0900

    fix: 로그인 DTO 수정

--- src/main/java/com/mmos/mmos/src/domain/dto/request/LoginRequestDto.java
+++ src/main/java/com/mmos/mmos/src/domain/dto/request/LoginRequestDto.java
@@ -2,9 +2,11 @@ package com.mmos.mmos.src.domain.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDto {
     String id;
     String pwd;
